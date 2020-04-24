54d72160b28ace922734f50aab97462de77ba0a1
Sonya Parau <sonyaparau@yahoo.com>
2020-04-24 21:58:15 +0300

    Refactoring and comments for Loading Screen.

--- lib/screens/loading.dart
+++ lib/screens/loading.dart
@@ -4,7 +4,8 @@ import 'package:reserve_it_app/screens/screenUtils/custom_widgets.dart';
 
 /*
 * Loading screen for actions that
-* take a while.
+* take a while to complete (like
+* Authentication).
 * */
 class Loading extends StatelessWidget {
   CustomWidgets _customWidgets = CustomWidgets();
@@ -12,35 +13,95 @@ class Loading extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: new AppBar(
-            title: Row(
-          children: <Widget>[
-            Image.asset(
-              'assets/app_logo.png',
-              fit: BoxFit.contain,
-              height: 32,
-              color: Colors.blueGrey,
-            ),
-            Container(
-                padding: const EdgeInsets.all(8.0), child: Text('ReserveIt'))
-          ],
-        )),
-        body: Container(
-          color: Colors.white,
-          child: Center(
-            child: new Column(
-              children: [
-                _customWidgets.getHeightSizedBox(250.0),
-                SpinKitFadingCircle(
-                  color: Colors.deepPurple,
-                  size: 100.0,
-                ),
-                _customWidgets.getHeightSizedBox(20.0),
-                Text('  Loading...',
-                    style: TextStyle(fontSize: 30.0, color: Colors.grey))
-              ],
-            ),
+      appBar: buildAppBar(),
+      body: Container(
+        color: Colors.white,
+        child: Center(
+          child: new Column(
+            children: [
+              _customWidgets.getHeightSizedBox(250.0),
+              buildSpinKitLoading(),
+              _customWidgets.getHeightSizedBox(25.0),
+              buildTextLoading(),
+              _customWidgets.getHeightSizedBox(140.0),
+              buildPhoto(),
+            ],
           ),
+        ),
+      ),
+    );
+  }
+
+  /*
+  * @return the text for the loading screen
+  * */
+  Text buildTextLoading() {
+    return Text('  Hello 😃',
+        style: TextStyle(
+          fontSize: 30.0,
+          color: Colors.grey,
         ));
+  }
+
+  /*
+  * @return the spin kit for loading
+  * */
+  SpinKitFadingCircle buildSpinKitLoading() {
+    return SpinKitFadingCircle(
+      color: Colors.deepPurple,
+      size: 100.0,
+    );
+  }
+
+  /*
+  * @return the AppBar for the loading screen
+  * */
+  AppBar buildAppBar() {
+    return AppBar(
+        title: Row(
+      children: <Widget>[
+        buildImageLogo(32.0),
+        buildContainerTitle(),
+      ],
+    ));
+  }
+
+  /*
+  * @return the text for the app bar
+  * */
+  Container buildContainerTitle() {
+    return Container(
+      padding: const EdgeInsets.all(8.0),
+      child: Text(
+        'Loading',
+      ),
+    );
+  }
+
+  /*
+  * @return the logo of the application
+  * */
+  Image buildImageLogo(double height) {
+    return Image.asset(
+      'assets/app_logo.png',
+      fit: BoxFit.contain,
+      height: height,
+      color: Colors.grey,
+    );
+  }
+
+  /*
+  * Creates the bottom of the loading screen containing
+  * the app's logo and name.
+  * */
+  buildPhoto() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        buildImageLogo(48.0),
+        _customWidgets.getWitdthSizedBox(2.0),
+        Text('ReserveIt', style: TextStyle(color: Colors.black54))
+      ],
+    );
   }
 }
